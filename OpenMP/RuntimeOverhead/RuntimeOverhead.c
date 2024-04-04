361122472fbe8bbecd4462f3874d088685d4b2b5
Yanfeng Du <145222768+yDu98@users.noreply.github.com>
2024-04-03 23:31:39 -0400

    Update RuntimeOverhead.c

--- OpenMP/RuntimeOverhead/RuntimeOverhead.c
+++ OpenMP/RuntimeOverhead/RuntimeOverhead.c
@@ -4,8 +4,8 @@
 
 int main() {
     int thread_count = 4; // Number of threads to use
-    LARGE_INTEGER frequency, start, end, work_start, work_end;
-    double creation_time, work_time, termination_time;
+    LARGE_INTEGER frequency, start, end, sync_start, sync_end;
+    double creation_time, sync_time, termination_time;
 
     QueryPerformanceFrequency(&frequency);
     
@@ -13,33 +13,43 @@ int main() {
 
     #pragma omp parallel num_threads(thread_count)
     {
-        if (omp_get_thread_num() == 0) {
-            // Get the time as soon as the first thread enters the parallel region
-            QueryPerformanceCounter(&work_start);
+        // Dummy work to prevent optimization out before synchronization
+        double pre_sum = 0;
+        for (int i = 0; i < 100000; ++i) {
+            pre_sum += i;
         }
 
-        // Dummy work to prevent optimization out
-        double sum = 0;
-        for (int i = 0; i < 10000000; ++i) {
-            sum += i;
+        int thread_num = omp_get_thread_num();
+        if (thread_num == 0) {
+            // Get the time before synchronization (only by the master thread)
+            QueryPerformanceCounter(&sync_start);
         }
+
+        // Synchronization point: all threads wait here
+        #pragma omp barrier
         
-        if (omp_get_thread_num() == 0) {
-            // Get the time when the first thread is about to finish the work
-            QueryPerformanceCounter(&work_end);
+        if (thread_num == 0) {
+            // Get the time after synchronization (only by the master thread)
+            QueryPerformanceCounter(&sync_end);
+        }
+
+        // Dummy work to prevent optimization out after synchronization
+        double post_sum = 0;
+        for (int i = 0; i < 100000; ++i) {
+            post_sum += i;
         }
     }
     
     QueryPerformanceCounter(&end); // End time
 
     // Calculate the time intervals
-    creation_time = (double)(work_start.QuadPart - start.QuadPart) / frequency.QuadPart;
-    work_time = (double)(work_end.QuadPart - work_start.QuadPart) / frequency.QuadPart;
-    termination_time = (double)(end.QuadPart - work_end.QuadPart) / frequency.QuadPart;
+    creation_time = (double)(sync_start.QuadPart - start.QuadPart) / frequency.QuadPart;
+    sync_time = (double)(sync_end.QuadPart - sync_start.QuadPart) / frequency.QuadPart;
+    termination_time = (double)(end.QuadPart - sync_end.QuadPart) / frequency.QuadPart;
 
     // Output the timing information
     printf("Thread creation time: %lf seconds\n", creation_time);
-    printf("Work (including synchronization) time: %lf seconds\n", work_time);
+    printf("Synchronization time: %lf seconds\n", sync_time);
     printf("Thread termination time: %lf seconds\n", termination_time);
 
     return 0;
